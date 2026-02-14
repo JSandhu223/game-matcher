f919aac126a1b52f6eb4744b1a5549bda796b79d
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2026-02-13 20:05:31 -0700

    Added basic functionality for retrieving friends list. For now, just returns the first friend from the friends list array.

--- Models/SteamModels.cs
+++ Models/SteamModels.cs
@@ -1,4 +1,6 @@
-﻿public class PlayerSummaryResponse
+﻿using System.Text.Json.Serialization;
+
+public class PlayerSummaryResponse
 {
     public PlayerContainer Response { get; set; } = new();
 }
@@ -13,4 +15,29 @@ public class PlayerSummary
     public string SteamId { get; set; } = "";
     public string PersonaName { get; set; } = "";
     public string AvatarFull { get; set; } = "";
+}
+
+
+public class FriendListResponse
+{
+    [JsonPropertyName("friendslist")]
+    public FriendListContainer Response { get; set; } = new();
+}
+
+public class FriendListContainer
+{
+    [JsonPropertyName("friends")]
+    public List<Friend> Friends { get; set; } = new();
+}
+
+public class Friend
+{
+    [JsonPropertyName("steamid")]
+    public string SteamId { get; set; } = "";
+
+    [JsonPropertyName("relationship")]
+    public string Relationship { get; set; } = "";
+
+    [JsonPropertyName("friend_since")]
+    public long FriendSince { get; set; }
 }
--- Services/SteamService.cs
+++ Services/SteamService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
+using System.Text.Json;
 
 public class SteamService
 {
@@ -13,11 +14,30 @@ public class SteamService
 
     public async Task<PlayerSummary?> GetPlayerSummaryAsync(string steamId)
     {
-        var url = $"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/" +
+        var url = $"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/" +
                   $"?key={_apiKey}&steamids={steamId}";
 
+        // The API returns a list of players, but since we're querying for a single Steam ID, we will take the first one.
+        // GetFromJSONAsync will automatically deserialize the JSON response into our PlayerSummaryResponse model.
         var response = await _httpClient.GetFromJsonAsync<PlayerSummaryResponse>(url);
+        var responseString = await _httpClient.GetStringAsync(url);
 
         return response?.Response.Players.FirstOrDefault();
+    }
+
+    public async Task<Friend?> GetFriendListAsync(string steamId)
+    {
+        var url = $"https://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={_apiKey}&steamid={steamId}&relationship=friend";
+
+        // Deserialize the JSON response into a list of Friend objects. The API returns a list of friends, but since we're querying for a single Steam ID, we will take the first one.
+        //var response = await _httpClient.GetFromJsonAsync<List<Friend>>(url);
+        var response = await _httpClient.GetFromJsonAsync<FriendListResponse>(url);
+        var responseString = await _httpClient.GetStringAsync(url);
+
+        // View the raw JSON response in the console for debugging purposes.
+        //var json = await _httpClient.GetFromJsonAsync<JsonDocument>(url);
+        //Console.WriteLine(json.RootElement.ToString());
+
+        return response?.Response.Friends.FirstOrDefault();
     }
 }
